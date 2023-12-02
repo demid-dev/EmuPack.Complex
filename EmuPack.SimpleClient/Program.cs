de119b8399fdfc30fedc49fd33184935429935d1
Demid-ns <46938257+Demid-ns@users.noreply.github.com>
2023-12-02 14:48:53 +0200

    Update launch profiles & add new command sequence in simple client project

--- EmuPack.SimpleClient/Program.cs
+++ EmuPack.SimpleClient/Program.cs
@@ -1,7 +1,13 @@
 ﻿using EmuPack.SimpleClient;
 
 Console.ReadKey();
-var tcpHttpClient = new EmulatorTcpClient("127.0.0.1", 8888, 100);
-string response = await tcpHttpClient.SendRequestAsync("INC1M100000");
-Console.WriteLine("Response: " + response);
+var tcpClient = new EmulatorTcpClient("127.0.0.1", 8888, 100);
+await tcpClient.SendRequestAsync("INC1M100000");
+await tcpClient.SendRequestAsync("SRC1M100000");
+await tcpClient.SendRequestAsync("MRC1M100005D0000");
+await tcpClient.SendRequestAsync("PRC1M100044I00010101Ibuprophen                    00003");
+await tcpClient.SendRequestAsync("FLC1M100015P0001A0MC010102");
+await tcpClient.SendRequestAsync("FLC1M100015P0001B0MC010101");
+await tcpClient.SendRequestAsync("MRC1M100005D0100");
+await tcpClient.SendRequestAsync("SRC1M100000");
 Console.ReadKey();
